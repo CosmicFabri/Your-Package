7ccfd197d9f589c4049b542a9f04cee46b510d91
Fabrizio Guzmán <fabrizzio86@outlook.es>
2024-11-20 22:35:26 -0600

    feat: Initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <body>
+    <!-- Bar that contains the logo -->
+    <div class="logo-bar">
+      <img src="./assets/logo-3.png" alt="Your Pack logo" id="logo-img">
+    </div>
+
+    <!-- Navigation bar for various options -->
+    <div class="navigation-bar">
+      [Various options, might be divs]
+    </div>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -19,8 +23,30 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0%;
+}
+
+body {
+  margin: 0px !important;
+}
+
+.logo-bar {
+  background-color: #c6e6fb;
+  height: 75px;
+  padding-left: 170px;
+  padding-right: 170px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.navigation-bar {
+  background-color: white;
+  height: 25px;
+  padding-left: 170px;
+  padding-right: 170px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  box-shadow: 0 4px 2px -2px gray;
 }
 </style>
